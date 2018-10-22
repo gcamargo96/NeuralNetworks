f3bafcfd42f935c084ff8a602adcf1c229766f54
gcamargo96 <gacamargo1.000@gmail.com>
2018-10-21 21:15:32 -0300

    Update projeto2.py

--- projeto2.py
+++ projeto2.py
@@ -4,26 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def transform_to_RGB(img_array):
-    return img_array.reshape(len(img_array), 3,32,32).transpose([0, 2, 3, 1])
-
-def normalize(x):
-    min_val = np.min(x)
-    max_val = np.max(x)
-    x = (x-min_val) / (max_val-min_val)
-    return x
-
-
 epochs = 10
 batch_size = 128
-channels = 3
-img_rows = 32
-img_cols = 32
 learning_rate = 0.001
-img_shape = (channels, img_rows, img_cols)
 
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.cifar10.load_data()
-
 
 num_classes = 10
 class_names =  ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
@@ -31,19 +16,19 @@ class_names =  ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 
 train_labels = keras.utils.to_categorical(train_labels, num_classes)
 test_labels = keras.utils.to_categorical(test_labels, num_classes)
 
-train_images = normalize(train_images)
-test_images = normalize(test_images)
+train_images = train_images.reshape(len(train_images), 3,32,32).transpose([0, 2, 3, 1])
+test_images = test_images.reshape(len(test_images), 3,32,32).transpose([0, 2, 3, 1])
 
 model = keras.Sequential([
-    keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape = (32, 32, 3) ),
+    keras.layers.Conv2D(64, kernel_size=(2, 2), activation='relu', input_shape = (32, 32, 3) ),
     keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2),
     keras.layers.BatchNormalization(),
 
-    keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
+    keras.layers.Conv2D(128, kernel_size=(2, 2), activation='relu'),
     keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2),
     keras.layers.BatchNormalization(),
 
-    keras.layers.Conv2D(256, kernel_size=(3, 3), activation='relu'),
+    keras.layers.Conv2D(256, kernel_size=(2, 2), activation='relu'),
     keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2),
     keras.layers.BatchNormalization(),
 
@@ -72,4 +57,4 @@ model.save("model.h5")
 
 test_loss, test_acc = model.evaluate(test_images, test_labels)
  
-print('Test accuracy:', test_acc)
+print("Test accuracy = ", test_acc)
